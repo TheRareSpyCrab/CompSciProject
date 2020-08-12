6663629efaeade4229b65aefb67eabb5b389fe9b
Arlo Hill <buffysnuffyrocks@gmail.com>
2020-08-13 09:09:24 +1200

    8th commit: Basic GUI, working on each fxml file, buttons no longer work

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,33 +1,33 @@
+
 package sample;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import java.io.IOException;
-
 
 public class Controller {
 
-
     @FXML
-
-
-    public void addScene(javafx.event.ActionEvent actionEvent)throws IOException {
-
-        Parent view2 = FXMLLoader.load(getClass().getResource("Museum.fxml"));
-
-        Scene scene2 =  new Scene(view2);
-
+    public void addScene2(ActionEvent actionEvent) throws IOException {
+        Parent view1 = (Parent) FXMLLoader.load(this.getClass().getResource("sample.fxml"));
+        Scene scene1 = new Scene(view1);
         Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-
-        window.setScene(scene2);
-
+        window.setScene(scene1);
         window.show();
-
     }
 
+    @FXML
+    public void addScene1(ActionEvent actionEvent) throws IOException {
+        Parent view2 = (Parent)FXMLLoader.load(this.getClass().getResource("Museum.fxml"));
+        Scene scene2 = new Scene(view2);
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(scene2);
+        window.show();
+    }
 }
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -19,21 +19,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("ACNH-Helper");
         primaryStage.setScene(new Scene(root, 1920, 1056));
         // to stop resizing otherwise all the pictures mess up O.o
         primaryStage.setResizable(false);
         primaryStage.show();
-
-
-
-
-
-
-
-
     }
 
-
-
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
