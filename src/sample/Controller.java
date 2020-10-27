7f2333d32dc0cebb0c0771b74a4ff2675b71795f
Arlo Hill <buffysnuffyrocks@gmail.com>
2020-10-28 11:35:09 +1300

    14th commit: Everything working in one FXML file, just have to be able to seralize file.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,110 +1,228 @@
 package sample;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import static java.lang.String.valueOf;
 
 public class Controller {
-    public static void main(String[] args) {
-        Map<String, String> Checkboxsave = new HashMap<String, String>();
 
-        Checkboxsave.put("Trevor Storr", "Ford");
-        Checkboxsave.put("John Doe", "Toyota");
-        Checkboxsave.put("Jane Doe", "Subaru");
+    @FXML
+    public javafx.scene.control.TextField textfield;
+    public javafx.scene.control.CheckBox check1;
+    public javafx.scene.control.CheckBox check2;
+    public javafx.scene.control.CheckBox check3;
+    public javafx.scene.control.CheckBox check4;
+    public javafx.scene.control.CheckBox check5;
+    public javafx.scene.control.CheckBox check6;
+    public javafx.scene.control.CheckBox check7;
+    public javafx.scene.control.CheckBox check8;
+    public javafx.scene.control.CheckBox check9;
+    public javafx.scene.control.CheckBox check10;
+    public javafx.scene.control.CheckBox check11;
+    public javafx.scene.control.CheckBox check12;
+    public javafx.scene.control.CheckBox check13;
+    public javafx.scene.control.CheckBox check14;
+    public javafx.scene.control.CheckBox check15;
+    public javafx.scene.control.CheckBox check16;
+    public javafx.scene.control.CheckBox check17;
+    public javafx.scene.control.CheckBox check18;
+    public javafx.scene.control.CheckBox check19;
+    public javafx.scene.control.CheckBox check20;
+    public javafx.scene.control.CheckBox check21;
+    public javafx.scene.control.CheckBox check22;
+    public javafx.scene.control.CheckBox check23;
+    public javafx.scene.control.CheckBox check24;
+    public javafx.scene.control.CheckBox check25;
+    public javafx.scene.control.CheckBox check26;
+    public javafx.scene.control.CheckBox check27;
+    public javafx.scene.control.CheckBox check28;
+    public javafx.scene.control.CheckBox check29;
+    public javafx.scene.control.CheckBox check30;
+    static int ticksp1 = 0;
 
-
-        for (Map.Entry me : Checkboxsave.entrySet()) {
-            System.out.println("Key: "+me.getKey() + " & Value: " + me.getValue());
-        }
-
-        String name = "Trevor Storr";
-        String car = Checkboxsave.get(name);
-        System.out.println (name + " owns a " + car  );
-
-        try
-        {
-            FileOutputStream fos =
-                    new FileOutputStream("ACNHsave.ser");
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(Checkboxsave);
-            oos.close();
-            fos.close();
-            System.out.printf("Saved");
-        }catch(IOException ioe)
-        {
-            ioe.printStackTrace();
-        }
+    public void PressButtonTicks(ActionEvent event){
+        System.out.println("amount of ticks is: ");
+        CountTheTicks();
     }
+    public void CountTheTicks(){
+        ticksp1 = 0;
+        if (check1.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check2.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check3.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check4.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check5.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check6.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check7.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check8.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check9.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check10.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check11.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check12.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check13.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check14.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check15.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check16.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check17.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check18.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check19.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check20.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check21.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check22.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check23.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check24.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check25.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check26.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check27.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check28.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check29.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        if (check30.isSelected()) {
+            ticksp1 = ticksp1 + 1;
+        }
+        countTotalTicks();
+    }
+    public void countTotalTicks(){
+        int TotalTicks = ticksp1;
+        System.out.println(TotalTicks);
+        textfield.setText(valueOf(TotalTicks + "/43"));
+    }
+
+
     // This controller is the main controller for switching windows, closing the program, and any other essential code that relates to each page
-
-    //Items button
     @FXML
-    public void addScene2(ActionEvent actionEvent) throws IOException {
-        Parent view1 = (Parent) FXMLLoader.load(this.getClass().getResource("Items.fxml"));
-        Scene scene1 = new Scene(view1);
-        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        window.setScene(scene1);
-        window.show();
-    }
-    //Museum button
+    public StackPane stackPane;
+    public Pane MainPane;
+    public Pane HomePane;
+    public ScrollPane MuseumPane1;
+    public ScrollPane MuseumPane2;
+    public ScrollPane MuseumPane3;
 
-    @FXML
-    public void addScene1(ActionEvent actionEvent) throws IOException {
-        Parent view2 = (Parent) FXMLLoader.load(this.getClass().getResource("Museum.fxml"));
-        Scene scene2 = new Scene(view2);
-        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        window.setScene(scene2);
-        window.show();
-    }
-    //villagers button
-
-    @FXML
-    public void addScene3(ActionEvent actionEvent) throws IOException {
-        Parent view3 = (Parent) FXMLLoader.load(this.getClass().getResource("Villagers.fxml"));
-        Scene scene3 = new Scene(view3);
-        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        window.setScene(scene3);
-        window.show();
-    }
-    //home button
-
-    @FXML
-    public void addScene4(ActionEvent actionEvent) throws IOException {
-        Parent view4 = (Parent) FXMLLoader.load(this.getClass().getResource("Home.fxml"));
-        Scene scene4 = new Scene(view4);
-        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        window.setScene(scene4);
-        window.show();
+    public void showPaneOne() {
+        MainPane.setVisible(true);
+        HomePane.setVisible(false);
+        MuseumPane1.setVisible(false);
+        MuseumPane2.setVisible(false);
+        MuseumPane3.setVisible(false);
     }
 
-    //settings button
-    @FXML
-    public void addScene5(ActionEvent actionEvent) throws IOException {
-        Parent view5 = (Parent) FXMLLoader.load(this.getClass().getResource("Settings.fxml"));
-        Scene scene5 = new Scene(view5);
-        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        window.setScene(scene5);
-        window.show();
+    public void showPanetwo() {
+        MainPane.setVisible(true);
+        HomePane.setVisible(false);
+        MuseumPane1.setVisible(false);
+        MuseumPane2.setVisible(false);
+        MuseumPane3.setVisible(false);
     }
-    //back button
 
-    @FXML
-    public void back1(ActionEvent actionEvent) throws IOException {
-        Parent home = (Parent) FXMLLoader.load(this.getClass().getResource("sample.fxml"));
-        Scene home2 = new Scene(home);
-        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        window.setScene(home2);
-        window.show();
+    public void showPaneOnethree() {
+        MainPane.setVisible(true);
+        HomePane.setVisible(false);
+        MuseumPane1.setVisible(false);
+        MuseumPane2.setVisible(false);
+        MuseumPane3.setVisible(false);
+    }
+    public void showPaneOnefour() {
+        MainPane.setVisible(true);
+        HomePane.setVisible(false);
+        MuseumPane1.setVisible(false);
+        MuseumPane2.setVisible(false);
+        MuseumPane3.setVisible(false);
+    }
+
+    public void showPaneTwo() {
+        MainPane.setVisible(false);
+        HomePane.setVisible(true);
+        MuseumPane1.setVisible(false);
+        MuseumPane2.setVisible(false);
+        MuseumPane3.setVisible(false);
+    }
+
+    public void showPaneThree() {
+        MainPane.setVisible(false);
+        HomePane.setVisible(false);
+        MuseumPane1.setVisible(true);
+        MuseumPane2.setVisible(false);
+        MuseumPane3.setVisible(false);
+    }
+
+    public void showPaneOFour() {
+        MainPane.setVisible(false);
+        HomePane.setVisible(false);
+        MuseumPane1.setVisible(false);
+        MuseumPane2.setVisible(true);
+        MuseumPane3.setVisible(false);
+    }
+
+    public void showPaneFive() {
+        MainPane.setVisible(false);
+        HomePane.setVisible(false);
+        MuseumPane1.setVisible(false);
+        MuseumPane2.setVisible(false);
+        MuseumPane3.setVisible(true);
     }
 
     //close button
@@ -118,32 +236,5 @@ public class Controller {
         stage.close();
     }
 
-    //museum tabs
 
-    @FXML
-    public void museumpaint1(ActionEvent actionEvent) throws IOException {
-        Parent museum = (Parent) FXMLLoader.load(this.getClass().getResource("Museum.fxml"));
-        Scene museum1 = new Scene(museum);
-        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        window.setScene(museum1);
-        window.show();
-    }
-
-    @FXML
-    public void museumpaint2(ActionEvent actionEvent) throws IOException {
-        Parent museum = (Parent) FXMLLoader.load(this.getClass().getResource("MuseumP2.fxml"));
-        Scene museum2 = new Scene(museum);
-        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        window.setScene(museum2);
-        window.show();
-    }
-
-    @FXML
-    public void museumpaint3(ActionEvent actionEvent) throws IOException {
-        Parent museum = (Parent) FXMLLoader.load(this.getClass().getResource("MuseumP3.fxml"));
-        Scene museum3 = new Scene(museum);
-        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        window.setScene(museum3);
-        window.show();
-    }
 }
